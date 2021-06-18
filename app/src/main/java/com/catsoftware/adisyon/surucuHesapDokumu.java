39251f831f95e3b25b9a24330c679738da72c1de
Ja Genau <jagenau49@gmail.com>
2021-06-18 14:16:36 +0200

    activity_surucu_hesap_dokumu.xml tasarlandi

---
+++ app/src/main/java/com/catsoftware/adisyon/surucuHesapDokumu.java
@@ -0,0 +1,14 @@
+package com.catsoftware.adisyon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class surucuHesapDokumu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surucu_hesap_dokumu);
+    }
+}
