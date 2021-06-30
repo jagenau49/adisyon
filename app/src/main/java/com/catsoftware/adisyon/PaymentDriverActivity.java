266f7b5d1151222b3f88bca250f1f53e4827db8d
Ja Genau <jagenau49@gmail.com>
2021-06-30 14:37:17 +0200

    PaymentDriverActivity.java is as english updated

--- app/src/main/java/com/catsoftware/adisyon/PaymentDriverActivity.java
+++ app/src/main/java/com/catsoftware/adisyon/PaymentDriverActivity.java
@@ -27,81 +27,83 @@ import java.util.List;
 import static com.catsoftware.adisyon.MainActivity.deleteOldOrders;
 
 public class PaymentDriverActivity extends AppCompatActivity {
-    public static final String LIST_OF_DRIVERS_ORDERS ="SORGU_SONUCU_LISTESI";
-    public static final String SELECTED_DRIVER ="SORGULANMIS_SURUCU_NO";
+    public static final String LIST_OF_DRIVERS_ORDERS ="LIST_OF_DRIVERS_ORDERS";
+    public static final String SELECTED_DRIVER ="SELECTED_DRIVER";
     public static final String KEY_LIST="LIST";
 
-    public static int saatIseBaslama = -1;
-    public static int saatIsiBitirme = -1;
-    public static int dakikaIseBaslama = -1;
-    public static int dakikaIsiBitirme = -1;
+    public static int hourBeginWork = -1;
+    public static int hourEndWork = -1;
+    public static int minuteBeginWork = -1;
+    public static int minuteEndWork = -1;
     public static String staticDriver;
-    Spinner spSurucuNo;
-    EditText etSaatlikUcreti;
-    Button btGeriDon, btHesapla, btPaketlerinDetaylariniGoster;
-    TextView tvTeslimEttigiPaketNakitUcreti, tvTeslimEttigiPaketKartUcreti, tvHakEttigiCalismaUcreti,
-            tvIadeEtmesiGerekenTutar, tvIsiBitirmeSaati, tvIseBaslamaSaati, tvSurucununToplamCalismaSaati,
-            tvSurucununTeslimEttigiPaketSayisi,tvSurucununToplamSaatlikGeliri,tvSurucuNoSorguSonucu;
+    Spinner spDrivers;
+    EditText etHourlyRate;
+    Button btBack, btCalculate, btShowDetails;
+    TextView tvTotalCashCostOfOrders, tvTotalCardCostOfOrders, tvDriversTodaysPaymet,
+            tvReturnAmount, tvEndHourOfWork, tvBeginHourOfWork, tvWorkHoursTotal,
+            tvDeliveredOrders, tvDriverGainedFromHourlyRate, tvDriverResultNumber;
 
-    LinearLayout linlayHesapSonuclari;
+    LinearLayout layoutResults;
     AppDatabase db;
-    List<Order> listeSurucununSiparisleri;
+    List<Order> orders;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_surucu_hesap_dokumu);
+        setContentView(R.layout.activity_payment_driver);
 
 
-        db = AppDatabase.getDbInstance(this.getApplicationContext());//veritabani baglaniyor
+        db = AppDatabase.getDbInstance(this.getApplicationContext());
 
-        //layout nesneleri ataniyor
-        spSurucuNo = findViewById(R.id.spSurucuNoHesaplik);
+        //layout items are assigned
+        spDrivers = findViewById(R.id.spDriversForPayment);
 
-        etSaatlikUcreti = findViewById(R.id.etSaatlikUcret);
-        btGeriDon = findViewById(R.id.btGeriDon);
-        btHesapla = findViewById(R.id.btHesapla);
-        btPaketlerinDetaylariniGoster=findViewById(R.id.btPaketlerinDetaylariniGoster);
-        linlayHesapSonuclari = findViewById(R.id.linlayHesapSonuclari);
-        tvTeslimEttigiPaketNakitUcreti = findViewById(R.id.tvTeslimEttigiPaketNakitUcreti);
-        tvTeslimEttigiPaketKartUcreti = findViewById(R.id.tvTeslimEttigiPaketKartUcreti);
-        tvHakEttigiCalismaUcreti = findViewById(R.id.tvSurcununCalismaUcreti);
-        tvIadeEtmesiGerekenTutar = findViewById(R.id.tvIadeEtmesiGerekenTutar);
-        tvIseBaslamaSaati = findViewById(R.id.tvIseBaslamaSaati);
-        tvIsiBitirmeSaati = findViewById(R.id.tvIsiBitirmeSaati);
-        tvSurucununToplamCalismaSaati = findViewById(R.id.tvSurucununToplamCalismaSaati);
-        tvSurucununTeslimEttigiPaketSayisi = findViewById(R.id.tvSurucununTeslimEttigiPaketSayisi);
-        tvSurucununToplamSaatlikGeliri=findViewById(R.id.tvSurucununToplamSaatlikGeliri);
-        tvSurucuNoSorguSonucu=findViewById(R.id.tvSurucuNoSorguSonucu);
-
-
+        etHourlyRate = findViewById(R.id.etHourlyRate);
+        btBack = findViewById(R.id.btBack);
+        btCalculate = findViewById(R.id.btCalculate);
+        btShowDetails =findViewById(R.id.btShowDetails);
+        layoutResults = findViewById(R.id.layoutResults);
+        tvTotalCashCostOfOrders = findViewById(R.id.tvTotalCashCostOfOrders);
+        tvTotalCardCostOfOrders = findViewById(R.id.tvTotalCardCostOfOrders);
+        tvDriversTodaysPaymet = findViewById(R.id.tvDriversTodaysPaymet);
+        tvReturnAmount = findViewById(R.id.tvReturnAmount);
+        tvBeginHourOfWork = findViewById(R.id.tvBeginHourOfWork);
+        tvEndHourOfWork = findViewById(R.id.tvEndHourOfWork);
+        tvWorkHoursTotal = findViewById(R.id.tvWorkHoursTotal);
+        tvDeliveredOrders = findViewById(R.id.tvDeliveredOrders);
+        tvDriverGainedFromHourlyRate =findViewById(R.id.tvDriverGainedFromHourlyRate);
+        tvDriverResultNumber =findViewById(R.id.tvDriverResultNumber);
 
 
-        //spinnera detaylar veriliyor
-        ArrayAdapter<CharSequence> adapterSurucuNo = ArrayAdapter.createFromResource(this,// Create an ArrayAdapter using the string array and a default spinner layout
+
+
+        //views updated
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.drivers, R.layout.spinner_item);
-        spSurucuNo.setAdapter(adapterSurucuNo);
-
-        //butonlara fonksiyon veriliyor
-        btGeriDon.setOnClickListener(v -> anaEkranaGit());
-        btPaketlerinDetaylariniGoster.setOnClickListener(v -> sorgulananSurucununPaketLeriniGoster());
-
-        btHesapla.setOnClickListener(v -> {
-
-            if (saatIseBaslama == 0 || saatIsiBitirme == 0 || etSaatlikUcreti.getText().toString().equals("")) {//eksik bilgiler var
-                Toast.makeText(PaymentDriverActivity.this, "Sorgulama yapilamadi! Lütfen eksik bilgileri giriniz.", Toast.LENGTH_LONG).show();
+        spDrivers.setAdapter(adapter);
 
 
-            } else {//bilgiler tam girilmis
+        btBack.setOnClickListener(v -> goToMainActivity());
+        btShowDetails.setOnClickListener(v -> showOrdersOfSelectedDriver());
+
+        btCalculate.setOnClickListener(v -> {
+
+            if (hourBeginWork == 0 || hourEndWork == 0 || etHourlyRate.getText().toString().equals("")) {
+                //missing inputs
+                Toast.makeText(PaymentDriverActivity.this, "Bitte den Fromular asufüllen! ", Toast.LENGTH_LONG).show();
+
+
+            } else {
+                //values are ready
 
 
 
-                odemeHesapla(spSurucuNo.getSelectedItem().toString(), saatIseBaslama,
-                        dakikaIseBaslama, saatIsiBitirme, dakikaIsiBitirme, Double.
-                                parseDouble(etSaatlikUcreti.getText().toString()));
+                calculatePayment(spDrivers.getSelectedItem().toString(), hourBeginWork,
+                        minuteBeginWork, hourEndWork, minuteEndWork, Double.
+                                parseDouble(etHourlyRate.getText().toString()));
 
-                //klavye gizleniyor
+                //hide keyboard
                 InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
                 inputMethodManager.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
             }
@@ -111,80 +113,79 @@ public class PaymentDriverActivity extends AppCompatActivity {
 
 
 
-    private void odemeHesapla(String surucuNo, int saatIseBaslama, int dakikaIseBaslama, int saatIsiBitirme, int dakikaIsiBitirme, double surucuSaatlikUcret) {//
+    private void calculatePayment(String driver, int hourAtBegin, int minuteAtBegin, int hourAtEnd, int minuteAtEnd, double hourlyRate) {//
         deleteOldOrders(PaymentDriverActivity.this);
-        staticDriver =surucuNo;
-         listeSurucununSiparisleri = db.orderDao().getAllOrdersOfDriver(surucuNo, false);
-        int surucununSiparisSayisi = listeSurucununSiparisleri.size();
-        System.out.println("" + surucuNo + ".surucunun siparis sayisi : " + surucununSiparisSayisi);
-        double surucudeToplananSiparisNakitUcreti = 0.0;
-        double surucudeToplananSiparisKartUcreti = 0.0;
-        int surucuToplamCalisma10min = sure10MinHesapla(saatIseBaslama, dakikaIseBaslama, saatIsiBitirme, dakikaIsiBitirme);
-        int surucununTeslimEttigiPaketSayisi = listeSurucununSiparisleri.size();
-        double surucununToplamSaatlikGeliri=((surucuSaatlikUcret / 6.0) * surucuToplamCalisma10min);
-        double surucununCalismaUcreti = surucununToplamSaatlikGeliri+ surucununTeslimEttigiPaketSayisi;
+        staticDriver =driver;
+         orders = db.orderDao().getAllOrdersOfDriver(driver, false);
+        int countDeliveredOrders = orders.size();
+        double sumOfDeliveredCashOrders = 0.0;
+        double sumOfDeliveredCardOrders = 0.0;
+        int sumOfDrivers10Mins = calculate10Mins(hourAtBegin, minuteAtBegin, hourAtEnd, minuteAtEnd);
 
-        for (Order siparisSatir : listeSurucununSiparisleri) {
-            if (siparisSatir.getPaymentMethod().equals("Online")) {
-                surucudeToplananSiparisKartUcreti += siparisSatir.getPrice();
-            } else if (siparisSatir.getPaymentMethod().equals("Bar")) {
-                surucudeToplananSiparisNakitUcreti += siparisSatir.getPrice();
+        double paymentForHourlyRates=((hourlyRate / 6.0) * sumOfDrivers10Mins);
+        double paymentTotalForDriver = paymentForHourlyRates+ countDeliveredOrders;
+
+        for (Order order : orders) {
+            if (order.getPaymentMethod().equals("Online")) {
+                sumOfDeliveredCardOrders += order.getPrice();
+            } else if (order.getPaymentMethod().equals("Bar")) {
+                sumOfDeliveredCashOrders += order.getPrice();
             }
 
         }
-        double surucununIadeEtmesiGerekenTutar = surucudeToplananSiparisNakitUcreti - surucununCalismaUcreti;
-        tvTeslimEttigiPaketNakitUcreti.setText(surucudeToplananSiparisNakitUcreti + "€");
-        tvTeslimEttigiPaketKartUcreti.setText(surucudeToplananSiparisKartUcreti + "€");
-        tvHakEttigiCalismaUcreti.setText(surucununCalismaUcreti + " €");
-        tvIadeEtmesiGerekenTutar.setText(surucununIadeEtmesiGerekenTutar + " €");
-        tvSurucununToplamCalismaSaati.setText(calismaSaatiHesapla(surucuToplamCalisma10min));
-        tvSurucununTeslimEttigiPaketSayisi.setText("" + surucununSiparisSayisi);
-        tvSurucununToplamSaatlikGeliri.setText("( "+surucununToplamSaatlikGeliri+"€ )");
-        tvSurucuNoSorguSonucu.setText(surucuNo);
+        double sumOfExtrasAtDriver = sumOfDeliveredCashOrders - paymentTotalForDriver;
+        tvTotalCashCostOfOrders.setText(sumOfDeliveredCashOrders + "€");
+        tvTotalCardCostOfOrders.setText(sumOfDeliveredCardOrders + "€");
+        tvDriversTodaysPaymet.setText(paymentTotalForDriver + " €");
+        tvReturnAmount.setText(sumOfExtrasAtDriver + " €");
+        tvWorkHoursTotal.setText(calculateWorkTime(sumOfDrivers10Mins));
+        tvDeliveredOrders.setText("" + countDeliveredOrders);
+        tvDriverGainedFromHourlyRate.setText("( "+paymentForHourlyRates+"€ )");
+        tvDriverResultNumber.setText(driver);
 
 
 
 
-        //sonuclar gorunur yapiliyor
-        linlayHesapSonuclari.setVisibility(View.VISIBLE);
+        //show results
+        layoutResults.setVisibility(View.VISIBLE);
 
 
     }
 
-    private String calismaSaatiHesapla(int toplam10MinAdedi) {
-        int toplamSaat = toplam10MinAdedi / 6;
-        int toplamDakika = (toplam10MinAdedi % 6) * 10;
-        return (toplamSaat + " Stunden " + toplamDakika + " Minuten");
+    private String calculateWorkTime(int sum10Mins) {
+        int totalHour = sum10Mins / 6;
+        int totalMinutes = (sum10Mins % 6) * 10;
+        return (totalHour + " Stunden " + totalMinutes + " Minuten");
 
     }
 
-    private int sure10MinHesapla(int saatIseBaslama, int dakikaIseBaslama, int saatIsiBitirme, int dakikaIsiBitirme) {
-        return ((60 - dakikaIseBaslama) + ((saatIsiBitirme - saatIseBaslama - 1) * 60) + dakikaIsiBitirme) / 10;
+    private int calculate10Mins(int hourBeginWork, int minutesBeginWork, int hourEndWork, int minuteEndWork) {
+        return ((60 - minutesBeginWork) + ((hourEndWork - hourBeginWork - 1) * 60) + minuteEndWork) / 10;
     }
 
-    public void anaEkranaGit() {
+    public void goToMainActivity() {
         Intent intent = new Intent(PaymentDriverActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void saatSec(View view) {
+    public void pickHour(View view) {
         // Get Current Time
-        int mHour = simdiSaatKac();
-        int mMinute = simdiDakikaKac();
+        int mHour = getCurrentHour();
+        int mMinute = getCurrentMinute();
 
         // Launch Time Picker Dialog
         TimePickerDialog timePickerDialog = new TimePickerDialog(this,
                 (timePicker, hourOfDay, minute) -> {
                     if (view.getId() == R.id.btIseBaslamaSaatiSec) {
-                        ((TextView) tvIseBaslamaSaati).setText(OrderAdapter.showAsTwoDigits(hourOfDay) + ":" + OrderAdapter.showAsTwoDigits(minute));
-                        ((TextView) tvIseBaslamaSaati).setVisibility(View.VISIBLE);
-                        saatIseBaslama = hourOfDay;
-                        dakikaIseBaslama = minute;
+                        ((TextView) tvBeginHourOfWork).setText(OrderAdapter.showAsTwoDigits(hourOfDay) + ":" + OrderAdapter.showAsTwoDigits(minute));
+                        ((TextView) tvBeginHourOfWork).setVisibility(View.VISIBLE);
+                        hourBeginWork = hourOfDay;
+                        minuteBeginWork = minute;
                     } else if (view.getId() == R.id.btIsiBitirmeSaatiSec) {
-                        ((TextView) tvIsiBitirmeSaati).setText(OrderAdapter.showAsTwoDigits(hourOfDay) + ":" + OrderAdapter.showAsTwoDigits(minute));
-                        ((TextView) tvIsiBitirmeSaati).setVisibility(View.VISIBLE);
-                        saatIsiBitirme = hourOfDay;
-                        dakikaIsiBitirme = minute;
+                        ((TextView) tvEndHourOfWork).setText(OrderAdapter.showAsTwoDigits(hourOfDay) + ":" + OrderAdapter.showAsTwoDigits(minute));
+                        ((TextView) tvEndHourOfWork).setVisibility(View.VISIBLE);
+                        hourEndWork = hourOfDay;
+                        minuteEndWork = minute;
                     }
 
 
@@ -192,25 +193,25 @@ public class PaymentDriverActivity extends AppCompatActivity {
         timePickerDialog.show();
     }
 
-    private int simdiDakikaKac() {
+    private int getCurrentMinute() {
         final Calendar c = Calendar.getInstance();
 
 
         return c.get(Calendar.MINUTE);
     }
 
-    private int simdiSaatKac() {
+    private int getCurrentHour() {
         final Calendar c = Calendar.getInstance();
 
         return c.get(Calendar.HOUR_OF_DAY);
     }
 
-    public void sorgulananSurucununPaketLeriniGoster() {
+    public void showOrdersOfSelectedDriver() {
         Intent i = new Intent(this, OrderDetailsOfDriver.class);
         Bundle b = new Bundle();
 
 
-        b.putSerializable(LIST_OF_DRIVERS_ORDERS, (Serializable) listeSurucununSiparisleri);
+        b.putSerializable(LIST_OF_DRIVERS_ORDERS, (Serializable) orders);
         i.putExtra(KEY_LIST,b);
         i.putExtra(SELECTED_DRIVER, staticDriver);
         startActivity(i);
